ff33a5a8619f27b4bf37d282f96deeceb1d0466e
Jonatanm92 <jonatanmurberger@gmail.com>
2026-08-19 16:00:47 +0200

    revenue: expose isolated public demo at /leadvakt

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { LeadVaktPublic } from './components/LeadVaktPublic';
 import './styles.css';
 import './company.css';
 import './lead-vakt.css';
+import './lead-vakt-public.css';
+
+const publicLeadVaktPath = window.location.pathname.replace(/\/+$/, '') === '/leadvakt';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {publicLeadVaktPath ? <LeadVaktPublic /> : <App />}
   </React.StrictMode>
 );
